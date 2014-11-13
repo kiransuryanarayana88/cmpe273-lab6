5401a7370aeaeb21b355c57ea5c10e47e7be6be9
kiransuryanarayana88 <kiran.suryanarayana88@gmail.com>
2014-11-12 22:56:51 -0800

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,4 +1,16 @@
 package edu.sjsu.cmpe.cache.client;
+import com.google.common.hash.Hashing;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/**
+ * CMPE 273- Lab 3
+ *          
+ * @version November, 2014
+ * 
+ * Author: Kiran Suryanarayana
+ */
 
 public class Client {
 
@@ -6,14 +18,30 @@ public class Client {
         System.out.println("Starting Cache Client...");
         CacheServiceInterface cache = new DistributedCacheService(
                 "http://localhost:3000");
+        CacheServiceInterface cache1 = new DistributedCacheService("http://localhost:3001");
+        CacheServiceInterface cache2 = new DistributedCacheService("http://localhost:3002");
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
+        List<CacheServiceInterface> server = new ArrayList(3);
+        server.add(cache);
+        server.add(cache1);
+        server.add(cache2);
+        
+        String [] value = {"0","a","b", "c", "d", "e", "f", "g", "h", "i", "j"};
+        
+        for(int key=1; key<11; key++) 
+        {
+            int collection = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), server.size());
+            server.get(collection).put(key, value[key]);
+            System.out.println(" Insert value - " + value[key] + " with KEY - " + key + " to SERVER - " + collection);
+        }
 
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+        for(int key=1; key<11; key++) 
+        {
+            int collection = Hashing.consistentHash(Hashing.md5().hashString(Integer.toString(key)), server.size());
+            System.out.println(" Retrieving value - " + server.get(collection).get(key) + " having KEY - " + key+ " from SERVER - "+ collection); 
+        }
+        
 
         System.out.println("Existing Cache Client...");
     }
-
 }
